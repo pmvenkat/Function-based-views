5041893bac0a90bf2537400c76d7005cbcc1fda9
Bhaskar <bhaskar@cowhite.com>
2014-10-30 17:15:54 +0530

    Views - using context - list of users

--- project_template/app1/views.py
+++ project_template/app1/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+
 
 def http_response(request):
 	return HttpResponse("This is a simple response !")
@@ -8,3 +10,11 @@ def http_response(request):
 
 def simple_template(request):
 	return render(request, "app1/simple_template.html", {})
+
+def users(request):
+	users = User.objects.all()
+	context = {
+		'users': users
+	}
+	return render(request, "app1/users.html", context)
+
